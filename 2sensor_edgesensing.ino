42b840d24e2a0249fa1472dd0709e760319a2bcc
Vidur Maheshwari <vidurm09@gmail.com>
2019-10-19 19:05:18 -0700

    Adding based 2 Sensor Code

---
+++ 2sensor_edgesensing.ino
@@ -0,0 +1,28 @@
+#include <SharpIR.h>
+
+SharpIR topSensor( SharpIR::GP2Y0A21YK0F, A0 );
+int topSensorDistance = 0;
+
+SharpIR bottomSensor( SharpIR::GP2Y0A21YK0F, A1);
+int bottomSensorDistance = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(100);  
+  
+  topSensorDistance = topSensor.getDistance();
+  bottomSensorDistance = bottomSensor.getDistance();
+
+  if (topSensorDistance > 55) {
+    tone(3, 500);
+  } else if (bottomSensorDistance > 55) {
+    tone(3, 500);
+  } else {
+    noTone(3);
+  }
+
+}
