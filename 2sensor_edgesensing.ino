11121278fae888d034a18fe12f301d7632e801c0
Shaunak Bhandarkar <shaunak.bhandarkar@gmail.com>
2019-11-21 21:12:30 -0800

    Changed to account for digital read values
    HIGH/LOW rather than 0-1023 from analog read

--- 2sensor_edgesensing.ino
+++ 2sensor_edgesensing.ino
@@ -1,14 +1,16 @@
 #include <SharpIR.h>
 
 int vibes;
-const int interval = 600; // in units of 100 ms
+const int interval = 60000; // in units of 100 ms
 int timer = interval;
+
+int milli = 0;
 
 
 SharpIR topSensor( SharpIR::GP2Y0A21YK0F, A0 );
 int topSensorDistance = 0;
 
-int vibrationPin = 5;
+int vibrationPin = 2;
 int outputPin1 = 7;
 int outputPin2 = 9;
 
@@ -24,23 +26,28 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(100);  
+  delay(1);  
+  milli++;
+  if (milli == 100) {
+    topSensorDistance = topSensor.getDistance();
+    bottomSensorDistance = bottomSensor.getDistance();
   
-  topSensorDistance = topSensor.getDistance();
-  bottomSensorDistance = bottomSensor.getDistance();
+    if (topSensorDistance > 55) {
+      tone(3, 500);
+    } else if (bottomSensorDistance > 55) {
+      tone(3, 500);
+    } else {
+      noTone(3);
+    }
 
-  if (topSensorDistance > 55) {
-    tone(3, 500);
-  } else if (bottomSensorDistance > 55) {
-    tone(3, 500);
-  } else {
-    noTone(3);
+    milli = 0;
   }
+  
 
   vibes = digitalRead(vibrationPin); // read vibration sensor, A0 and A1 taken
   
   
-  if (vibes < 1000){ //reads up to 1023, >1000 means no motion
+  if (vibes == HIGH){ //reads up to 1023, >1000 means no motion
     timer = interval;
     digitalWrite(outputPin1, HIGH);
     digitalWrite(outputPin2, HIGH);
